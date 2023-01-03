9a5d639f0b62690f9fc92ae03a6e4d3401a80382
Fabricio Magnus <fabricio.magnus92@outlook.com>
2023-01-03 10:35:39 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Fabricio";
+  let title = "";
+  let image = "https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
+  let description = "";
+</script>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
