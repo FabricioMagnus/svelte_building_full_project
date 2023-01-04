3292979de1a4975a10ffbc7166ea634c90d93d33
Fabricio Magnus <fabricio.magnus92@outlook.com>
2023-01-04 10:16:54 -0300

    add: loops, events and functions

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,40 @@
   let title = "";
   let image = "https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
   let description = "";
+  let formState = "empty";
+
+  let createdContact = [];
+
+  function addContact() {
+    if (
+      name.trim().length === 0 ||
+      title.trim().length === 0 ||
+      image.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      formState = "invalid";
+      return;
+    }
+    formState = "done";
+    createdContact = [
+      ...createdContact,
+      {
+        id: Math.random(),
+        name: name,
+        jobTitle: title,
+        imageURL: image,
+        desc: description,
+      },
+    ];
+  }
+
+  function deleteFirst() {
+    createdContact = createdContact.slice(1);
+  }
+
+  function deleteLast() {
+    createdContact = createdContact.slice(0, -1);
+  }
 </script>
 
 <div id="form">
@@ -26,7 +60,24 @@
   </div>
 </div>
 
-<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+<button on:click={addContact}>Add Contact Card</button>
+<button on:click={deleteFirst}>Delete First Card</button>
+<button on:click={deleteLast}>Delete Last Card</button>
+
+{#if formState === "invalid"}
+  <p>Invalid input</p>
+{/if}
+{#each createdContact as contact, index (contact.id)}
+  <h2># {index}</h2>
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    description={contact.desc}
+    userImage={contact.imageURL}
+  />
+{:else}
+  <p>Card list is empty!</p>
+{/each}
 
 <style>
   #form {
